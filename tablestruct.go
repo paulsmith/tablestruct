fd5d339c80f0b2955aad5772b9d2614737aafd13
Paul Smith <paulsmith@pobox.com>
2014-07-01 23:17:00 -0400

    Fix bug

--- tablestruct.go
+++ tablestruct.go
@@ -21,7 +21,7 @@ type Map []TableMap
 // NewMap constructs a new mapping object.
 func NewMap(in io.Reader) (*Map, error) {
 	var mapper Map
-	if err := json.NewDecoder(os.Stdin).Decode(&mapper); err != nil {
+	if err := json.NewDecoder(in).Decode(&mapper); err != nil {
 		return nil, err
 	}
 	return &mapper, nil
